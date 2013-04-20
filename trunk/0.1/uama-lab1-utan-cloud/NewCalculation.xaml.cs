7f89bf07589f22eab6498a2d04cf85cb47360edf
gabriel.holmer@gmail.com <gabriel.holmer@gmail.com@06a9e77b-5dc6-e798-a84f-548be4cc49b2>
2013-04-20 13:08:04 +0000

    Fungerande ScheduledAgent.

--- trunk/0.1/uama-lab1-utan-cloud/NewCalculation.xaml.cs
+++ trunk/0.1/uama-lab1-utan-cloud/NewCalculation.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define DEBUG_AGENT
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,6 +20,8 @@ namespace uama_lab1_utan_cloud
 {
     public partial class NewCalculation : PhoneApplicationPage
     {
+        PeriodicTask periodicTask;
+
         public NewCalculation()
         {
             InitializeComponent();
@@ -47,9 +51,61 @@ namespace uama_lab1_utan_cloud
 
             citiesToVisitListBox.Items.CopyTo(citiesToVisit, 0);
 
-            TSPScheduledAgent tspAgent = new TSPScheduledAgent();
+            StartPeriodicAgent();
+        }
 
+        private void StartPeriodicAgent()
+        {
+            // Obtain a reference to the period task, if one exists
+            periodicTask = ScheduledActionService.Find("tsp") as PeriodicTask;
 
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule
+            if (periodicTask != null)
+            {
+                RemoveAgent("tsp");
+            }
+
+            periodicTask = new PeriodicTask("tsp");
+
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            periodicTask.Description = "This demonstrates a periodic task.";
+
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(periodicTask);
+
+                // If debugging is enabled, use LaunchForTest to launch the agent in ten seconds.
+                #if(DEBUG_AGENT)
+                    ScheduledActionService.LaunchForTest("tsp", TimeSpan.FromSeconds(10));
+                #endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                }
+
+                if (exception.Message.Contains("BNS Error: The maximum number of ScheduledActions of this type have already been added."))
+                {
+                    // No user action required. The system prompts the user when the hard limit of periodic tasks has been reached.
+                }
+            }
+        }
+
+        private void RemoveAgent(string name)
+        {
+            try
+            {
+                ScheduledActionService.Remove(name);
+            }
+            catch (Exception)
+            {
+            }
         }
 
         private void allCitiesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
