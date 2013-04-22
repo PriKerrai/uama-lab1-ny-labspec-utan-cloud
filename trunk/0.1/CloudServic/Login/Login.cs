c557d21040749122a23a1f1d23aa0854c790bd0d
karljsjo@gmail.com <karljsjo@gmail.com@06a9e77b-5dc6-e798-a84f-548be4cc49b2>
2013-04-22 11:19:33 +0000

    login services addedddd

---
+++ trunk/0.1/CloudServic/Login/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CloudServic.Login
+{
+    public class Login
+    {
+        LoginDB loginDB = LoginDB.Instance;
+
+        public bool CreateUser(User user)
+        {
+            if (loginDB.users.Contains(user))
+            {
+                return false;
+            }
+            else
+            {
+                loginDB.users.Add(user);
+                return true;
+            }
+        }
+
+        public bool LoginUser(User user)
+        {
+            return loginDB.users.Contains(user) ? true: false;
+        }
+    }
+}
---
+++ trunk/0.1/CloudServic/Login/LoginDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace CloudServic.Login
+{
+    public class LoginDB
+    {
+        private static LoginDB instance;
+
+        public static LoginDB Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LoginDB();
+                }
+                return instance;
+            }
+        }
+
+        public List<User> users;
+    }
+}
